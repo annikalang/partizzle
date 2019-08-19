aaa916e219c09952d26df81d356a752b63eff6a8
juan solano <juanpablosolanosuarez@hotmail.com>
2019-08-19 09:25:54 -0700

    added seeds and the location index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,21 @@
 class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def new
+    @locations = Location.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
 end
