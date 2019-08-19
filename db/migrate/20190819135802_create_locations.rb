8a6da55855b909717b3abfe1fdebc979089ed0b2
Annika Lang <Lang-annika@t-online.de>
2019-08-19 16:00:28 +0200

    generated location model

---
+++ db/migrate/20190819135802_create_locations.rb
@@ -0,0 +1,14 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.string :address
+      t.string :title
+      t.text :description
+      t.integer :size
+      t.integer :price
+      t.text :tags, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
