bf9fcdfe85328135b35b553ed8003af86a518d0f
juan solano <juanpablosolanosuarez@hotmail.com>
2019-08-19 09:33:12 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,6 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 puts 'Creating restaurants...'
 user = User.create(email:"juansolano1034@gmail.com",password:"123456",phone_number: "4157699904",first_name:"Juan",last_name:"Solano")
 user1 = User.create(email:"annika1034@gmail.com",password:"1234567",phone_number: "4157699908",first_name:"Annika",last_name:"Lang")
