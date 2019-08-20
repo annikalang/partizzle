9b032c67b5d26c06b7291948d8acd8890eaa49bd
juan solano <juanpablosolanosuarez@hotmail.com>
2019-08-20 05:25:17 -0700

    locations-controller-working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,18 +4,49 @@ class LocationsController < ApplicationController
   end
 
   def new
-    @locations = Location.new
+    @location = Location.new
+  end
+
+  def show
+    @location = Location.find(params[:id])
   end
 
   def create
-
+    @user = current_user
+    @location = Location.new(locations_params)
+    @location.user = @user
+    if @location.save!
+      redirect_to location_path(@location)
+    else
+      render :new
+    end
   end
 
   def edit
+    @location = Location.find(params[:id])
+  end
 
+  def update
+    @location = Location.find(params[:id])
+    @location.update(locations_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      render :edit
+    end
   end
 
   def destroy
+   @location = Location.find(params[:id])
+   @location.destroy
+   redirect_to root_path
+  end
 
+  def set_params
+    @location = Location.find(params[:id])
+  end
+
+   def locations_params
+     params.require(:location).permit(:title, :address, :description, :size, :price, :tags, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'locations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :locations, only: [:index, :show, :new, :create] do
+  resources :locations, only: [:index, :show, :new, :create, :edit, :update] do
     resources :bookings, only: [:create]
   end
+  resources :locations, only: [:destroy]
 end
