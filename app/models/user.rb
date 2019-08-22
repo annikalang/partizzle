6fed33f80de859be1ac367302d2e25fe03d0c057
Annika Lang <Lang-annika@t-online.de>
2019-08-22 11:21:42 +0200

    added dependent destroy to user and location

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :locations
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :phone_number, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
